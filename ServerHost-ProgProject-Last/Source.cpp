826a22ba14ffa90a6cd41fde3526a9a2eabf69eb
WWalter <walter.wanerskog2@gmail.com>
2024-04-22 13:18:36 +0200

    Local works on same device, testing diferent

--- ServerHost-ProgProject-Last/Source.cpp
+++ ServerHost-ProgProject-Last/Source.cpp
@@ -1,5 +1,66 @@
 #include <iostream>
+#include <winsock2.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
 int main() {
-	std::cout << "Hello world!\n-David, 2022" << std::endl;
-	return 0;
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cerr << "WSAStartup failed\n";
+        return 1;
+    }
+
+    SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (serverSocket == INVALID_SOCKET) {
+        std::cerr << "Error creating socket: " << WSAGetLastError() << "\n";
+        WSACleanup();
+        return 1;
+    }
+
+    sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(12345); // Choose a port
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(serverSocket, reinterpret_cast<sockaddr*>(&serverAddr), sizeof(serverAddr)) == SOCKET_ERROR) {
+        std::cerr << "Error binding socket: " << WSAGetLastError() << "\n";
+        closesocket(serverSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    if (listen(serverSocket, 5) == SOCKET_ERROR) {
+        std::cerr << "Error listening on socket: " << WSAGetLastError() << "\n";
+        closesocket(serverSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    std::cout << "Server listening for incoming connections...\n";
+
+    sockaddr_in clientAddr;
+    int clientAddrSize = sizeof(clientAddr);
+    SOCKET clientSocket = accept(serverSocket, reinterpret_cast<sockaddr*>(&clientAddr), &clientAddrSize);
+    if (clientSocket == INVALID_SOCKET) {
+        std::cerr << "Error accepting connection: " << WSAGetLastError() << "\n";
+        closesocket(serverSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    char buffer[1024];
+    int bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
+    if (bytesReceived == SOCKET_ERROR) {
+        std::cerr << "Error receiving data: " << WSAGetLastError() << "\n";
+    }
+    else {
+        buffer[bytesReceived] = '\0';
+        std::cout << "Received message: " << buffer << "\n";
+    }
+
+    closesocket(clientSocket);
+    closesocket(serverSocket);
+    WSACleanup();
+
+    return 0;
 }
